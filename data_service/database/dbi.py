b818a30d5dd782d56d50e9993a64b505d2e8d7bc
张冬 <zhangd@lhcis.com>
2019-08-07 23:12:45 +0800

    [add] user dbi

--- data_service/database/dbi.py
+++ data_service/database/dbi.py
@@ -40,7 +40,60 @@ class Dbi(database.Base):
                 "message": "Select success",
                 "user": {
                     "id": user.id,
-                    "password": user.password
+                    "username": user.username,
+                    "password": user.password,
+                    "is_active": user.is_active,
+                    "is_superuser": user.is_superuser,
+                    "email": user.email,
+                    "phone": user.phone,
+                    "realname": user.realname,
+                    "avatar": user.avatar,
+                    "service_serial": user.service_serial,
+                    "is_modify_username": user.is_modify_username,
+                    "is_vip": user.is_vip
+                }
+            }
+            return msg
+        except Exception as e:
+            msg = {
+                "code": constant.ErrCode.ERR_UNKNOWN,
+                "message": str(e)
+            }
+            return msg
+        finally:
+            session.close()
+
+    def select_user_by_id(self, user_id):
+        """
+        Select user info by user id
+        :param user_id: [int] User table id
+        :return:
+        """
+        session = self.__session()
+        try:
+            user = session.query(models.User).filter(models.User.id == user_id).first()
+            if user is None:
+                msg = {
+                    "code": constant.ErrCode.ERR_USERNAME_NOT_FOUND,
+                    "message": "Username not found."
+                }
+                return msg
+            msg = {
+                "code": constant.ErrCode.ERR_OK,
+                "message": "Select success",
+                "user": {
+                    "id": user.id,
+                    "username": user.username,
+                    "password": user.password,
+                    "is_active": user.is_active,
+                    "is_superuser": user.is_superuser,
+                    "email": user.email,
+                    "phone": user.phone,
+                    "realname": user.realname,
+                    "avatar": user.avatar,
+                    "service_serial": user.service_serial,
+                    "is_modify_username": user.is_modify_username,
+                    "is_vip": user.is_vip
                 }
             }
             return msg
